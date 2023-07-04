7c9a0e4c3393082a9a2d156390784ac0caa578fa
Kailash Choudhary <kailashjat604@gmail.com>
2023-07-04 22:00:11 +0530

    Fix image paths

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import { NavLink } from "react-router-dom";
 import "../css/Navbar.css";
+import LogoImage from "../assets/logo.png";
 
 function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-left">
-        <img src="/public/logo.png" alt="Logo" className="logo-image" />
+        <img src={LogoImage} alt="Logo" className="logo-image" />
       </div>
       <div className="navbar-right">
         <NavLink to=".">Home</NavLink>
