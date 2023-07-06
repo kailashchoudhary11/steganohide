577b539a4337e5971ff5c585b4c0015ad99ed6c1
Kailash Choudhary <kailashjat604@gmail.com>
2023-07-06 22:45:37 +0530

    Setup jwt token views

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from .views import Endpoints, HideText, RevealText
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 app_name = 'api'
 
@@ -7,4 +11,8 @@ urlpatterns = [
     path('', Endpoints.as_view()),
     path('hide/', HideText.as_view(), name='hide-text'),
     path('reveal/', RevealText.as_view(), name='reveal-text'),
+
+    # JWT Authentication
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
