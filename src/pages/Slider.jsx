214ca98d6de587ba49b01a0b598b3f1cd6689421
Shreya Dhiman <shreyanksha2004@gmail.com>
2023-07-11 01:33:45 +0530

    slider

--- src/pages/Slider.jsx
+++ src/pages/Slider.jsx
@@ -1,72 +1,66 @@
-import React, { useEffect } from 'react';
+
 import '../css/slider.css';
 
 export default function SliderForm() {
-  useEffect(() => {
-    const signUpButton = document.getElementById('signUp');
-    const signInButton = document.getElementById('signIn');
-    const container = document.getElementById('container');
 
-    const handleSignUp = () => {
-      container.classList.add('right-panel-active');
-    };
-
-    const handleSignIn = () => {
-      container.classList.remove('right-panel-active');
-    };
-
-    signUpButton.addEventListener('click', handleSignUp);
-    signInButton.addEventListener('click', handleSignIn);
-
-    return () => {
-      signUpButton.removeEventListener('click', handleSignUp);
-      signInButton.removeEventListener('click', handleSignIn);
-    };
-  }, []);
 
   return (
     <>
-      <div className="container" id="container">
-        <div class="form-container sign-up-container">
-          <form action="#">
-            <h1>Create Account</h1>
-            <div class="social-container">
-              <a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
-              <a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
-              <a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
+      <section class="main">
+        <div class="form_wrapper">
+          <input type="radio" class="radio" name="radio" id="login" checked />
+          <input type="radio" class="radio" name="radio" id="signup" />
+          <div class="tile">
+            <h3 class="login">Login Form</h3>
+            <h3 class="signup">Signup Form</h3>
+          </div>
+
+          <label class="tab login_tab" for="login">
+            Login
+          </label>
+
+          <label class="tab signup_tab" for="signup">
+            Signup
+          </label>
+          <span class="shape"></span>
+          <div class="form_wrap">
+            <div class="form_fild login_form">
+              <div class="input_group">
+                <input type="email" class="input" placeholder="Email Address" />
+              </div>
+              <div class="input_group">
+                <input type="password" class="input" placeholder="Password" />
+              </div>
+
+              <a href="#forgot" class="forgot">Forgot password?</a>
+
+              <input type="submit" class="btn" value="Login" />
+
+              <div class="not_mem">
+                <label for="signup">Not a member? <span> Signup now</span></label>
+              </div>
+
             </div>
-            <span>or use your email for registration</span>
-            <input type="text" placeholder="Name" />
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <button>Sign Up</button>
-          </form>
-        </div>
-        <div class="form-container sign-in-container">
-          <form action="#">
-            <h1>Sign in</h1>
-            <span>or use your account</span>
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <a href="#">Forgot your password?</a>
-            <button>Sign In</button>
-          </form>
-        </div>
-        <div class="overlay-container">
-          <div class="overlay">
-            <div class="overlay-panel overlay-left">
-              <h1>Welcome Back!</h1>
-              <p>To keep connected with us please login with your personal info</p>
-              <button class="ghost" id="signIn">Sign In</button>
-            </div>
-            <div class="overlay-panel overlay-right">
-              <h1>Hello, Friend!</h1>
-              <p>Enter your personal details and start journey with us</p>
-              <button class="ghost" id="signUp">Sign Up</button>
+
+            <div class="form_fild signup_form">
+              <div class="input_group">
+                <input type="email" class="input" placeholder="Email Address" />
+              </div>
+              <div class="input_group">
+                <input type="password" class="input" placeholder="Password" />
+              </div>
+
+              <div class="input_group">
+                <input type="password" class="input" placeholder="Confirm Password" />
+              </div>
+
+              <input type="submit" class="btn" value="Signup" />
+
             </div>
           </div>
+
         </div>
-      </div>
+      </section>
     </>
   );
 }
